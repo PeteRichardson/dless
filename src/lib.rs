6a51d791658db84daf8ec429316e51af842532cf
Pete Richardson <pete@peterichardson.com>
2024-05-20 10:41:39 -0700

    Read arg with clap crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
-#[derive(Debug)]
+use clap::Parser;
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
 pub struct DlessConfig {
-    pub file: String,
+    /// log file to view
+    #[arg(default_value = "testdata/dlog0.log")]
+    pub file: PathBuf,
 }
 
-pub fn dless(config: DlessConfig) -> usize {
-    println!("Hello, {}!", config.file);
-    0
+pub fn dless(config: DlessConfig) -> ExitCode {
+    println!("Hello, {:}!", config.file.display());
+    ExitCode::SUCCESS
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
+use clap::Parser;
 use dless::{dless, DlessConfig};
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
-    let config = DlessConfig {
-        file: "testdata/dlog0.log".to_string(),
-    };
-    if dless(config) == 0 {
-        ExitCode::SUCCESS
-    } else {
-        ExitCode::FAILURE
-    }
+    let config = DlessConfig::parse();
+    dless(config)
 }
